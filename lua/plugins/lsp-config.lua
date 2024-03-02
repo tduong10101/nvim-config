2de6c14abce57f78657eac8630ebef1693aafbeb
terry duong <tduong10101@gmail.com>
2024-03-03 00:46:22 +1100

    added go config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pylsp" },
+				ensure_installed = { "lua_ls", "pylsp", "gopls" },
 			})
 		end,
 	},
@@ -18,12 +18,30 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
+      local util = require "lspconfig/util"
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
 			lspconfig.pylsp.setup({
 				capabilities = capabilities,
 			})
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+        cmd = {"gopls"},
+        filetypes = {
+          "go", "gomod", "gowork", "gotmpl"
+        },
+        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+              unusedparams = true,
+            },
+          },
+        },
+      })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,10 @@ return {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.pylint
+        null_ls.builtins.diagnostics.pylint,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.golines,
+        null_ls.builtins.formatting.goimports_reviser
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
