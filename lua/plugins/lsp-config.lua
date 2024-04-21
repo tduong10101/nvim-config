317d520ef30d98bab3e3387eb753557c570a3111
terry duong <tduong10101@gmail.com>
2024-04-21 21:13:45 +1000

    update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,8 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pylsp", "gopls", "jdtls" },
+				--ensure_installed = { "lua_ls", "pylsp", "gopls", "jdtls" },
+        automatic_installation = true,
 			})
 		end,
 	},
