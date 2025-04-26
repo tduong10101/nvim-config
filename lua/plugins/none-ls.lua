a4ac91b71669f54ca56d8fa5eb8e6718b33357df
tduong10101 <tduong10101@gmail.com>
2025-04-26 17:31:30 +1000

    update config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,6 +1,10 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     local null_ls = require("null-ls")
 
     null_ls.setup({
@@ -8,14 +12,29 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
-        null_ls.builtins.diagnostics.pylint,
-        null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.golines,
         null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.terraform_fmt,
+        null_ls.builtins.diagnostics.pylint,
+        --require("none-ls.diagnostics.biome"),
       },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({
+            group = augroup,
+            buffer = bufnr,
+          })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
   end,
