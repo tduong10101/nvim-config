bef910dd3b0e96d63fb7d51be0156f7de9700b1f
tduong10101 <tduong10101@gmail.com>
2025-04-30 20:16:54 +1000

    update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,13 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config=function()
-    require('lualine').setup({
-      options = {
-        theme = 'nightfly'
-      }
-  })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "nightfly",
+				component_separators = "",
+				section_separators = "",
+			},
+		})
+	end,
 }
